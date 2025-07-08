986bad8e1df45b17505021aba27157a5f8141431
regulorapo <regulorapo@gmail.com>
2025-07-08 19:03:01 -0400

    Create DashboardPage.tsx

---
+++ src/pages/src/pages/src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div className="dashboard">
+      <h1>Welcome to Nexora Boost Dashboard</h1>
+      <p>This is where your referrals, analytics, and controls will appear.</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
