a6471054a734ed4b026713d8d3b8d231cf091596
regulorapo <regulorapo@gmail.com>
2025-07-08 18:55:58 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
-import React from 'react'; import './App.css'; export default function App() { return <div className='app'>Welcome to Nexora Boost 🚀</div>; }
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
